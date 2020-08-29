558ad5f31fc4ad94c334ae21cb50cc2afa6c84d3
Vasutp <vasutp@zerobit.co.th>
2020-08-29 13:32:11 +0700

    - Update Scene - Create Interactable Prefabs

---
+++ ghost_simulator/Assets/Scripts/PotPlantInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class PotPlantInteraction : ItemInteraction
+{
+    private  Rigidbody _rigidbody;
+    
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        interactionName = "KickOver";
+    }
+
+
+    public override void Execute()
+    {
+        _rigidbody.AddForce(-transform.forward*3, ForceMode.Impulse);
+    }
+}
---
+++ ghost_simulator/Assets/Scripts/PullOverObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PullOverObject : ItemInteraction
+{
+
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _strength = 1;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        interactionName = "Pull Over";
+    }
+    
+    public override void Execute()
+    {
+        _rigidbody.AddForce(transform.forward * _strength, ForceMode.Impulse);      
+    }
+}
