79bf052be2f87c3c3f5422e9826487fd44b1d6a7
Vasutp <vasutp@zerobit.co.th>
2020-08-30 15:33:56 +0700

    - Reset Timescale to 1 on  LoadMenu

--- ghost_simulator/Assets/AnotherPauseMenu.cs
+++ ghost_simulator/Assets/AnotherPauseMenu.cs
@@ -5,7 +5,7 @@ using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine.SceneManagement;
 public class AnotherPauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    public static bool isPause = false;
     public GameObject pauseMenuUI;
 
     private FirstPersonController firstPersonController;
@@ -16,25 +16,39 @@ public class AnotherPauseMenu : MonoBehaviour
     }
 
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
+        { 
+            if (isPause)
             {
+                HideCursor();
                 Resume();
             } else
             {
+                ShowCursor();
                 Pause();
             }
         }
+    }
+
+    private void HideCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+
+    private void ShowCursor()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
     }
 
     public void Resume ()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        GameIsPaused = false;
+        isPause = false;
         firstPersonController.IsPause = false;
     }
 
@@ -42,12 +56,13 @@ public class AnotherPauseMenu : MonoBehaviour
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        GameIsPaused = true;
+        isPause = true;
         firstPersonController.IsPause = true;
     }
 
     public void LoadMenu ()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
         firstPersonController.IsPause = false;
     }
