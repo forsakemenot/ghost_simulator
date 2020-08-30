5439fe82b3def9b08ae095cc12e39dbf2a73c007
Vasutp <vasutp@zerobit.co.th>
2020-08-30 13:02:25 +0700

    - Add Basic ScoreSystem

---
+++ ghost_simulator/Assets/Scripts/ScoreSystem/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ScoreSystem
+{
+    public class ScoreDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI scoreTextMesh;
+
+        private void Start()
+        {
+            scoreTextMesh = GetComponentInChildren<TextMeshProUGUI>();
+            scoreTextMesh.text = "Score : 0";
+        }
+
+        public void UpdateScoreDisplay(float currentScore)
+        {
+            scoreTextMesh.text = "Score : "+currentScore;
+        }
+        
+        
+    }
+}
